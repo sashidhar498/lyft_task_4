d26725ba6e201398b4ab691d1e6cf69b61333a84
sashidhar498 <74296659+sashidhar498@users.noreply.github.com>
2023-05-09 18:22:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from engines.capulet_engine import CapuletEngine
 from engines.sternman_engine import SternmanEngine
 from engines.willoughby_engine import WilloughbyEngine
 from factory import create_car
+from tires.Carrigan import carrigan
+from tires.Octoprime import octoprime
 from datetime import datetime
 
 
@@ -11,29 +13,34 @@ class all_cars():
     def calliope(self,current_date, last_service_date, current_mileage, last_service_mileage):
         engine = CapuletEngine(current_mileage, last_service_mileage)
         battery = spindler(current_date, last_service_date)
-        car=create_car(engine, battery)
+        tire=carrigan(tires)
+        car=create_car(engine, battery,tires)
         return car.check_service()
 
     def glissade(self,current_date, last_service_date, current_mileage, last_service_mileage):
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
         battery = spindler(current_date, last_service_date)
-        car=create_car(engine, battery)
+        tire=carrigan(tires)
+        car=create_car(engine, battery,tires)
         return car.check_service()
 
     def palindrome(self,current_date, last_service_date, warning_light_is_on):
         engine = SternmanEngine(warning_light_is_on)
         battery = spindler(current_date, last_service_date)
-        car=create_car(engine, battery)
+        tire=carrigan(tires)
+        car=create_car(engine, battery,tires)
         return car.check_service()
 
     def rorschach(self,current_date, last_service_date, current_mileage, last_service_mileage):
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
         battery = nubbin(current_date, last_service_date)
-        car=create_car(engine, battery)
+        tire=octoprime(tires)
+        car=create_car(engine, battery,tires)
         return car.check_service()
 
     def thovex(self,current_date, last_service_date, current_mileage, last_service_mileage):
         engine = CapuletEngine(current_mileage, last_service_mileage)
         battery = nubbin(current_date, last_service_date)
-        car=create_car(engine, battery)
+        tire=octoprime(tires)
+        car=create_car(engine, battery,tires)
         return car.check_service()
